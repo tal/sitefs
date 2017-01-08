aa1a66fa2d0b13204305789af308766ede92425f
Tal Atlas <me@tal.by>
2017-01-08 18:41:06 -0500

    Add sourcemaps to sass

--- lib/sitefs/handlers/scss.rb
+++ lib/sitefs/handlers/scss.rb
@@ -3,14 +3,26 @@ require 'sass'
 class Sitefs::Handlers::Scss < Sitefs::Handler
 
   def content
-    options = {
+    engine.render
+  end
+
+  def engine_options
+    {
       cache: true,
       syntax: :scss,
       style: :compressed,
       filename: source_file,
+      sourcemap: :inline,
+      load_paths: [root_path],
     }
+  end
 
-    Sass::Engine.new(File.read(source_file), options).render
+  def engine
+    @engine ||= Sass::Engine.new(File.read(source_file), engine_options)
+  end
+
+  def render_with_sourcemap
+    @render_with_sourcemap ||= engine.render_with_sourcemap(sourcemap_output_pathname)
   end
 
   # def delay_generation
@@ -25,9 +37,27 @@ class Sitefs::Handlers::Scss < Sitefs::Handler
     source_file.sub(/\.scss$/, '.css')
   end
 
+  def output_pathname
+    File.join('', output_path.sub(root_path, ''))
+  end
+
+  def sourcemap_output_pathname
+    output_pathname + '.map'
+  end
+
+  def sourcemap_output_path
+    output_path + '.map'
+  end
+
   def file_actions registry
-    FileAction.new path: output_path do
-      content
-    end
+    sourcemap_options = {
+      css_path: output_path,
+      sourcemap_path: sourcemap_output_path,
+    }
+
+    [
+      FileAction.new(path: output_path) { render_with_sourcemap[0] },
+      FileAction.new(path: sourcemap_output_path) { render_with_sourcemap[1].to_json(sourcemap_options) },
+    ]
   end
 end
